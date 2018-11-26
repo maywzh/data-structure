1f5dfa9ca7999bb995c44ba05ed59ab19da9b9d6
maywzh <maywzh@gmail.com>
2018-11-27 03:31:45 +0800

    BFS

--- src/graph/BFS.c
+++ src/graph/BFS.c
@@ -1,5 +1,28 @@
 #include "ArcNode.h"
 #include "MGraph.h"
-void BFS(AGraph *G, int v, int visit[MAXSIZE]){
-    
+#include <stdlib.h>
+void BFS(AGraph *G, int v, int visit[MAXSIZE])
+{
+    int que[MAXSIZE];
+    int front = 0, rear = 0;
+    VNode_Visit(v);
+    visit[v] = 1;
+    rear = (rear + 1) % MAXSIZE;
+    que[rear] = v;
+    while (front != rear)
+    {
+        front = (front + 1) % MAXSIZE;
+        ArcNode *p = G->adjlist[que[front]].firstarc;
+        while (p != NULL)
+        {
+            if (visit[p->adjvex]==0)
+            {
+                VNode_Visit(p->adjvex);
+                visit[p->adjvex] = 1;
+                rear = (rear + 1) % MAXSIZE;
+                que[rear] = p->adjvex;
+            }
+            p = p->nextarc;
+        }
+    }
 }
