bac7872cb0a93d0144c0423b13720fe7a75084f5
maywzh <maywzh@gmail.com>
2018-11-28 04:48:11 +0800

    base

--- src/stack/listack.cxx
+++ src/stack/listack.cxx
@@ -16,6 +16,19 @@ void push(LNode *lst, int x)
 {
     LNode *p;
     p = (LNode *)malloc(sizeof(LNode));
+    p->data = x;
     p->next = lst->next;
     lst->next = p;
+}
+
+int pop(LNode *lst, int &x)
+{
+    LNode *p;
+    if (lst->next == NULL)
+        return 0;
+    p = lst->next;
+    x = p->data;
+    lst->next = p->next;
+    free(p);
+    return 1;
 }
