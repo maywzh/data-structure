b4e1f6d804ae32a6c5a1db781444c5496a2b9c73
maywzh <maywzh@gmail.com>
2018-11-30 02:24:30 +0800

    KMP

--- src/string/KMP.cxx
+++ src/string/KMP.cxx
@@ -1,6 +1,38 @@
 #include "str.h"
 #include <stdlib.h>
-void getnext(Str substr, int next[])
+void getnext(String substr, int next[])
 {
+    int i = 1,
+        j = 0;
+    next[1] = 0;
+    while (i < substr.length)
+    {
+        if (j == 0 || substr.ch[i] == substr.ch[j])
+        {
+            ++i;
+            ++j;
+            next[i] = j;
+        }
+        else
+            j = next[j];
+    }
+}
 
+int KMP(String str, Str substr, int next[])
+{
+    int i = 1, j = 1;
+    while (i <= str.length && j <= substr.length)
+    {
+        if (j == 0 || str.ch[i] == substr.ch[j])
+        {
+            ++i;
+            ++j;
+        }
+        else
+            j = next[j];
+    }
+    if (j > substr.length)
+        return i - substr.length;
+    else
+        return 0;
 }
