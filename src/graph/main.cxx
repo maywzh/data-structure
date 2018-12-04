b442a5df88ab6819a05a03985943dd1792d99257
maywzh <maywzh@gmail.com>
2018-12-04 17:25:11 +0800

    mian.xcc

--- src/graph/main.cxx
+++ src/graph/main.cxx
@@ -33,12 +33,28 @@ AGraph *MG2AG(MGraph *mg)
     AGraph *ag = (AGraph *)malloc(sizeof(AGraph));
     ag->n = mg->n;
     ag->e = mg->e;
+    int flag = 0;
+    ArcNode *p;
     for (int i = 0; i < ag->n; i++)
     {
-        for (int j = 0; j < ag->n; j++)
+        flag = 0;
+        for (int j = 0; j < ag->n && j != i; j++)
         {
-            if (mg->edges[i][j] < __INT32_MAX__ && i != j){
-                
+            if (mg->edges[i][j] < __INT32_MAX__ && i != j)
+            {
+                if (flag == 0)
+                {
+                    flag = 1;
+                    ag->adjlist[i].firstarc->adjvex = j;
+                    ag->adjlist[i].firstarc->info = mg->edges[i][j];
+                    p = ag->adjlist[i].firstarc;
+                }
+                else
+                {
+                    p->nextarc->adjvex = j;
+                    p->nextarc->info = mg->edges[i][j];
+                    p = p->nextarc;
+                }
             }
         }
     }
