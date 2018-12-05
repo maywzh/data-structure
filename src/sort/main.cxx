07ffe8daa0b36e6b5fe09c91c20bf8992cab8102
maywzh <maywzh@gmail.com>
2018-12-05 21:25:01 +0800

    main

--- src/sort/main.cxx
+++ src/sort/main.cxx
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main()
 {
-    int A[9] = {0, 49, 38, 65, 97, 76, 13, 27, 49};
+    int A[8] = {49, 38, 65, 97, 76, 13, 27, 49};
     HeapSort(A, 8);
     for (int i = 1; i < 9; i++)
     {
